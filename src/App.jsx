b40347eebff16ee3f7cec94e2903fcf03503211d
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-01-15 18:01:01 +0100

    Aggiunta logica per salvare i dati degli input su App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import Header from './components/Header'
-import InputGroup from './components/InputGroup'
+import {useState} from 'react';
+import Header from './components/Header';
+import InputGroup from './components/InputGroup';
 function App() {
+  // "alzo" questo stato nell'app component perchè l'userInput mi 
+  // servirà nei calcoli per stampare gli eventuali risultati.
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 5000,
+    annualInvestment: 1000,
+    expectedReturn: 8,
+    duration: 5,
+  })
+  function inputHandle(key, value){
+    // il prevUserInput perchè in react è meglio non modificare direttamente
+    // la variabile iniziale ma crearne una copia
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [key]: value, // sintassi di js per cambiare la singola coppia chiave/valore
+      }
+      
+    })
+    
+  }
   return (
     <>
       <Header/>
-      <InputGroup/>
+      <InputGroup inputHandle={inputHandle} userInput={userInput}/>
     </>
   )
 }
--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,24 +1,32 @@
-export default function InputGroup(){
+export default function InputGroup({inputHandle, userInput}){
     return(
         <section id="user-input">
             <div className="input-group">
                 <div>
                     <label>Investimento iniziale</label>
-                    <input type="text" />
+                    <input type="number" required 
+                    value={userInput.initialInvestment} 
+                    onChange={(event)=> inputHandle('initialInvestment', event.target.value)} />
                 </div>
                 <div>
                     <label>Investimento annuale</label>
-                    <input type="text" />
+                    <input type="number" required 
+                    value={userInput.annualInvestment} 
+                    onChange={(event)=> inputHandle('annualInvestment', event.target.value)}/>
                 </div>
             </div>
             <div className="input-group">
                 <div>
-                    <label>Profitto previsto</label>
-                    <input type="text" />
+                    <label>Profitto previsto percentuale</label>
+                    <input type="number" required  
+                    value={userInput.expectedReturn} 
+                    onChange={(event)=> inputHandle('expectedReturn', event.target.value)}/>
                 </div>
                 <div>
                     <label>Durata</label>
-                    <input type="text" />
+                    <input type="number" required min='1' 
+                    value={userInput.duration} 
+                    onChange={(event)=> inputHandle('duration', event.target.value)} />
                 </div>
             </div>
         </section>
